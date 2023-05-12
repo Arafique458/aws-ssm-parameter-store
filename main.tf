860ddc2bea97d14c38e52372ac70e7cd8927c7b6
arafique458 <arafique458@gmail.com>
2023-05-11 22:57:25 -0400

    Moved under roles

--- main.tf
+++ main.tf
@@ -2,12 +2,7 @@ resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
   name = var.parameters[count.index].name
   value = var.parameters[count.index].value
-  overwrite = false
+  overwrite = true
   type = "String"
   key_id = "801d82e5-19a3-4b80-88c6-5947b7b90508"
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
 }
