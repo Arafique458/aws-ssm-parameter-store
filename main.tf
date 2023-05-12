1e14861ec6a4112c2accdf628df71de783dcd517
arafique458 <arafique458@gmail.com>
2023-05-11 22:53:06 -0400

    Moved under roles

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
   name = var.parameters[count.index].name
   value = var.parameters[count.index].value
-  type = "SecureString"
+  type = "String"
   key_id = "801d82e5-19a3-4b80-88c6-5947b7b90508"
-
 }
