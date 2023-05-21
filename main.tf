427f9b73585c9d82f09699fc0e457e944c0a633f
arafique458 <arafique458@gmail.com>
2023-05-20 23:32:51 -0400

    payment and payment.ser  ansible configuration set.

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_ssm_parameter" "parameter" {
+resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name = var.parameters[count.index].name
   value = var.parameters[count.index].value
   overwrite = true # will over-write the existing parameter and add any more.
-  type = "SecureString"
+  type = "String"
   key_id = "46fec639-a30b-4dcc-a7b0-c94cc0b7810f"
 }
